0447b18e55a8df215cdc948478a68ca27394e011
M B <mbagnsk@hotmail.com>
2023-03-18 22:04:37 +0100

    creating all tables via python script

--- src/raspberry-pi-python/database configuration/main.py
+++ src/raspberry-pi-python/database configuration/main.py
@@ -32,12 +32,32 @@ def create_table(conn, create_table_sql):
 
 def main():
     database = r"measurementsDB.db"
-    create_measurementstype_table = """CREATE TABLE IF NOT EXISTS measurementsType(idMeasurementType INTEGER PRIMARY KEY 
-                                        AUTO INCREMENT, measurementType TEXT NOT NULL);"""
+    _create_measurementstype_table = """CREATE TABLE IF NOT EXISTS measurementsType(
+                                        idMeasurementType INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                        measurementType TEXT NOT NULL);"""
+    _create_iothubs_table = """CREATE TABLE IF NOT EXISTS iotHubs(
+                                idIotHub INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                iotHub TEXT NOT NULL);"""
+    _create_devices_table = """CREATE TABLE IF NOT EXISTS devices(
+                                idDevice INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                device TEXT NOT NULL, 
+                                description TEXT);"""
+    _create_temperature_table = """CREATE TABLE IF NOT EXISTS temperature(
+                                iotHub INTEGER, 
+                                idDevice INTEGER, 
+                                idChannel INTEGER, 
+                                value REAL NOT NULL, 
+                                datetime TEXT NOT NULL, 
+                                FOREIGN KEY(iotHub) REFERENCES iotHubs(idIotHub), 
+                                FOREIGN KEY(idDevice) REFERENCES devices(idDevice), 
+                                PRIMARY KEY (iotHub, idDevice, idChannel))"""
 
     conn = create_connection(database)
     if conn is not None:
-        create_table(conn, create_measurementstype_table)
+        create_table(conn, _create_measurementstype_table)
+        create_table(conn, _create_iothubs_table)
+        create_table(conn, _create_devices_table)
+        create_table(conn, _create_temperature_table)
     else:
         print("Error, cannot connect to database.")
 
