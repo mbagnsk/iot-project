91d83bace39739b202726fa78e70cb6076e7a219
M B <mbagnsk@hotmail.com>
2023-03-19 13:14:19 +0100

    new database structure

--- src/raspberry-pi-python/database configuration/main.py
+++ src/raspberry-pi-python/database configuration/main.py
@@ -55,17 +55,25 @@ def main():
                                 device TEXT NOT NULL, 
                                 description TEXT);"""
     _create_temperature_table = """CREATE TABLE IF NOT EXISTS temperature(
-                                iotHub INTEGER NOT NULL, 
+                                idIotHub INTEGER NOT NULL, 
                                 idDevice INTEGER NOT NULL, 
-                                idChannel INTEGER NOT NULL, 
+                                idChannel INTEGER NOT NULL,
                                 value REAL NOT NULL, 
                                 datetime TEXT NOT NULL, 
-                                FOREIGN KEY(iotHub) REFERENCES iotHubs(idIotHub), 
+                                FOREIGN KEY(idIotHub) REFERENCES iotHubs(idIotHub), 
                                 FOREIGN KEY(idDevice) REFERENCES devices(idDevice), 
-                                PRIMARY KEY (iotHub, idDevice, idChannel))"""
-    _insert_measurementtype_init = """INSERT INTO measurementsType (measurementType) VALUES('temperature')"""
-    _insert_iothub_init = """INSERT INTO iotHubs (iotHub) VALUES ('Raspberry Pi 3B')"""
-    _insert_devices_init = """INSERT INTO devices (device) VALUES ('ESP8266')"""
+                                PRIMARY KEY (idIotHub, idDevice, idChannel))"""
+    _create_units_table = """CREATE TABLE IF NOT EXISTS units(
+                            idUnit INTEGER PRIMARY KEY AUTOINCREMENT,
+                            idMeasurementType INTEGER NOT NULL,
+                            unitName TEXT NOT NULL,
+                            unit TEXT NOT NULL,
+                            FOREIGN KEY(idMeasurementType) REFERENCES measurementsType(idMeasurementType));"""
+
+    _insert_measurementtype_init = """INSERT INTO measurementsType (measurementType) VALUES('temperature');"""
+    _insert_iothub_init = """INSERT INTO iotHubs (iotHub) VALUES ('Raspberry Pi 3B');"""
+    _insert_devices_init = """INSERT INTO devices (device) VALUES ('ESP8266');"""
+    _insert_units_init = """INSERT INTO units (idMeasurementType, unitName, unit) VALUES(1, 'Celsius', 'C');"""
 
     conn = create_connection(database)
     if conn is not None:
@@ -73,9 +81,11 @@ def main():
         create_table(conn, _create_iothubs_table)
         create_table(conn, _create_devices_table)
         create_table(conn, _create_temperature_table)
+        create_table(conn, _create_units_table)
         insert_data(conn, _insert_measurementtype_init)
         insert_data(conn, _insert_iothub_init)
         insert_data(conn, _insert_devices_init)
+        insert_data(conn, _insert_units_init)
     else:
         print("Error, cannot connect to database.")
 
