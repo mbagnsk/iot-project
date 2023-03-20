0528f454c761e6e1d29676b2fa4445785382951a
M B <mbagnsk@hotmail.com>
2023-03-20 17:25:31 +0100

    decoding json format in python

--- src/raspberry-pi-python/subscriber/subscriber.py
+++ src/raspberry-pi-python/subscriber/subscriber.py
@@ -1,14 +1,22 @@
 import paho.mqtt.client as mqtt
 import sys
+import json
 
 topic = sys.argv[1]
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: " + str(rc))
     client.subscribe(topic)
 
+
 def on_message(client, userdata, msg):
-    print(str(msg.payload))
+    try:
+        message_received = json.loads(msg.payload)
+        print(message_received["message"]["measurement"]["measurementType"])
+    except ValueError:
+        print("Decoding JSON has failed")
+
 
 client = mqtt.Client()
 client.connect("192.168.1.54", 1884, 60)
